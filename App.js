e48a37d551f04a18c13c110f292cbdec073fc55d
Mohamed ElGhamry <mohamedhanyelghamry@gmail.com>
2025-05-15 15:25:08 +0300

    Mohamed elghamry

--- App.js
+++ App.js
@@ -4,19 +4,18 @@ import StudentDashboard from './pages/StudentDashboard';
 import ApplyPage from './pages/ApplyPage';
 import ApplicationsPage from './pages/ApplicationsPage';
 import ProfilePage from './pages/ProfilePage';
-import LoginPage from './pages/LoginPage' ;
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<StudentDashboard />} />
-        <Route path="/apply" element={<ApplyPage />} />
-        <Route path="/applications" element={<ApplicationsPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-      </Routes>
-    </Router>
-  );
-}
+import LoginPage from './pages/LoginPage';
 
-export default App;
+function App() {
+return (
+<Router>
+<Routes>
+<Route path="/" element={<LoginPage />}/>
+<Route path="/dashboard" element={<StudentDashboard />} />
+<Route path="/apply" element={<ApplyPage />} />
+<Route path="/applications" element={<ApplicationsPage />} />
+<Route path="/profile" element={<ProfilePage />} />
+</Routes>
+</Router>
+);
+}
