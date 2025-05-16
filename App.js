8245e81e81939ae7431bf65c46a2b16b1d245eb3
nadiemismail1 <nadeemahmedismailshehata@gmail.com>
2025-05-16 12:08:56 +0300

    Update App.js

--- App.js
+++ App.js
@@ -3,11 +3,16 @@ import Loginsignup from './loginsignup.jsx';
 import Dashboard from './dashboard.jsx';
 import Uploader from './uploader.jsx';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import Notifications from './Notifications';
+import ApplicationsList from './ApplicationsList.jsx';
+import ApplicationDetails from './ApplicationDetails';
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/applications" exact component={ApplicationsList} />
+        <Route path="/applications/:id" component={ApplicationDetails} />
+        <Route path="/notifications" component={Notifications} />
         <Route exact path="/" component={Loginsignup} />
         <Route path="/dashboard" component={Dashboard} />
         <Route path="/uploader" component={Uploader} />
